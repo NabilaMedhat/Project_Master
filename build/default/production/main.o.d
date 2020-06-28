b6091a52e1b6c5e367a557b4a0c7d0e33baca851
NabilaMedhat <63113362+NabilaMedhat@users.noreply.github.com>
2020-06-29 00:22:26 +0200

    Second Commit

--- build/default/production/main.o.d
+++ build/default/production/main.o.d
@@ -1,5 +1,7 @@
 build/default/production/main.o.d build/default/production/main.o: main.c \
- c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\interrupt.h \
+ USART.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\xc.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\cci.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\io.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\sfr_defs.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\inttypes.h \
@@ -11,18 +13,20 @@ build/default/production/main.o.d build/default/production/main.o: main.c \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\version.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\interrupt.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\util\delay.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\math.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\stdlib.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stddef.h \
  c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\string.h \
- USART.h \
- c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\xc.h \
- c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\cci.h \
  SPI_Master.h
 
-c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\interrupt.h:
+USART.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\xc.h:
+
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\cci.h:
 
 c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\io.h:
 
@@ -46,6 +50,8 @@ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\fuse.h:
 
 c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\lock.h:
 
+c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\avr\interrupt.h:
+
 c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\util\delay.h:
 
 c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\util\delay_basic.h:
@@ -57,11 +63,5 @@ c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\stdlib.h:
 c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\stddef.h:
 
 c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\avr\include\string.h:
-
-USART.h:
-
-c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\xc.h:
-
-c:\program\ files\ (x86)\microchip\xc8\v2.10\avr\lib\gcc\avr\5.4.0\include\cci.h:
 
 SPI_Master.h:
--- main.c
+++ main.c
@@ -5,103 +5,52 @@
  * Created on May 22, 2020, 1:12 AM
  */
 
-
-#define F_CPU 8000000UL
-
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <avr/io.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "USART.h"
 #include "SPI_Master.h"
 
-/*ISR(USART_RXC_vect) {
-
-    char Rec = UDR;
-    if (Rec == 'O') {
-        PORTA |= (1 << LED);
-        USART_TransmitString(str1);
-        SPI_write('O');
-
-    } else if (Rec == 'C') {
-        PORTA &= ~(1 << LED);
-        USART_TransmitString(str2);
-        SPI_write('C');
-    }
-    //USART_TransmitChar(Rec);
-
-}*/
-char *Buffer;
 char O1[] = "O1 ";
 char C1[] = "C1 ";
 char O2[] = "O2 ";
 char C2[] = "C2 ";
 
-//char msg[] = "Temp = ";
 
 char str1[] = "LED 1 is ON\t\r";
 char str2[] = "LED 1 is OFF\t\r";
 char str3[] = "LED 2 is ON\t\r";
 char str4[] = "LED 2 is OFF\t\r";
 
-char Rec[1000];
+char Welcome[] = "Please type 'O1 #' or 'O2 #' or 'C1 #' or 'C2 #' to choose\t\r";
+char Buffer[20];
 
 int main(void) {
     DDRA |= (1 << LED);
-    USART_init(); // Enable Transmitter ONLY
+    USART_init();
     SPI_Master_init();
-    /* Replace with your application code */
-    //sei();
+    USART_TransmitString(Welcome);
     while (1) {
-        /* if (PINC & (1 << 0)) {
-             USART_TransmitChar('A');
-             USART_TransmitString(msg);
-             USART_TransmitValue(20);
-             USART_NewLine();
-             _delay_ms(500);
-             //SPI_write('A');
-             //SPI_write(counter++);
-             //_delay_ms(1000);*/
-
-        USART_ReceiveString(Rec);
-        if (checkData(Rec, O1)) {
+        USART_ReceiveString(Buffer);
+        if (checkData(Buffer, O1)) {
             PORTA |= (1 << LED);
             USART_NewLine();
             USART_TransmitString(str1);
             SPI_WriteString(O1);
-        } else if (checkData(Rec, C1)) {
+        } else if (checkData(Buffer, C1)) {
             PORTA &= ~(1 << LED);
             USART_NewLine();
             USART_TransmitString(str2);
             SPI_WriteString(C1);
-        } else if (checkData(Rec, O2)) {
+        } else if (checkData(Buffer, O2)) {
             PORTA |= (1 << LED);
             USART_NewLine();
             USART_TransmitString(str3);
             SPI_WriteString(O2);
-        } else if (checkData(Rec, C2)) {
+        } else if (checkData(Buffer, C2)) {
             PORTA &= (1 << LED);
             USART_NewLine();
             USART_TransmitString(str4);
             SPI_WriteString(C2);
         }
-        /*
-        char Rec = UDR;
-                if (Rec == 'O') {
-                    PORTA |= (1 << LED);
-                    USART_TransmitString(str1);
-                    //SPI_Write('O');
-                    SPI_WriteString(O1);
 
-                } else if (Rec == 'C') {
-                    PORTA &= ~(1 << LED);
-                    USART_TransmitString(str2);
-                    //SPI_Write('C');
-                    SPI_WriteString(C1);
-                }
-         */
     }
 }
 
